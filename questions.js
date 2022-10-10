683de9ae7dcd93f8cc0c7cee7799e9c0fa4cd94f
MakuSantiran <73550264+MakuSantiran@users.noreply.github.com>
2022-10-10 23:21:22 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,264 @@
+//var drainBy = -0.0005;
+//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS
+
+question = ["It acts in an environment, perceives its environment through sensors and it can change its environment as well.","it can be looked upon as a system that implements a mapping from percept sequences to actions.","Autonomous agent decides autonomously which action to take in the current situation to maximize progress towards its goals."];
+answer = ["Agent"];
+group = "Definition";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+//  fasfaa // afsaagdgad //
+
+question = ["It specifies which actions an agent ought to take at any point in time."];
+answer = ["Ideal mapping"];
+group = "Agent Performance";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["Is a subjective measure to characterize how successful an agent is."];
+answer = ["Performance Measure"];
+group = "Agent Performance";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+
+///
+
+question = ["An agent that always does the right thing."];
+answer = ["Rational Agent"];
+group = "Definition";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["It must sense, must act, must be autonomous (to some extent)"];
+answer = ["Intelligent Agent"];
+group = "Definition";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+///
+
+question = ["Assumes that the rational agent knows all and will take the action that maximizes its utility."];
+answer = ["Perfect Rationality"];
+group = "Rationality";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["Is the action that maximizes the expected value of the performance measure given the percept sequence to date."];
+answer = ["Rational Action"];
+group = "Rationality";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+////
+
+question = ["An environment can be characterized as fully observable or partially observable."];
+answer = ["Observability"];
+group = "Agent Environment";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["it means, all of the environment relevant to the action being considered is observable."];
+answer = ["Fully observable"];
+group = "Agent Environment";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["The relevant features of the environment are only partially observable."];
+answer = ["Partially Observable"];
+group = "Agent Environment";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["the next state of the environment is completely described by the current state and the agent's action"];
+answer = ["Deterministic"];
+group = "Agent Environment";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["the next state of the environment is completely unpredictable by the current state and the agent's action"];
+answer = ["Stochastic"];
+group = "Agent Environment";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["An episodic environment means that subsequent episodes do not depend on what actions occurred in previous episodes."];
+answer = ["Episodic"];
+group = "Agent Environment";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["Its an environment where the agent engages in a series of connected episodes."];
+answer = ["Sequential"];
+group = "Agent Environment";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["If the number of distinct percepts and actions is limited, the environment is what?"];
+answer = ["Discrete"];
+group = "Agent Environment";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["If the number of distinct percepts and actions is non countable, the environment is called what?"];
+answer = ["Continuous"];
+group = "Agent Environment";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["Its all about Single agent or Multi-agent"];
+answer = ["Presence of other agents"];
+group = "Agent Environment";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+/////
+
+question = ["The action is looked up from a table based on information about the agent’s percepts."];
+answer = ["Table Based Agents"];
+group = "Agent Architectures";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["Its an agent architecture where its information comes from sensors and changes the agent's current state of the world.. it triggers actions throught the actuators"];
+answer = ["Reflex Agent"];
+group = "Agent Architectures";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["There is no explicit knowledge representation, its behaviour is distributed, not centralized and response to stimuli is reflexive."];
+answer = ["Brooks Architecture","Subsumption Architecture"];
+group = "Agent Architectures";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["A state-based agent works as information comes from sensors, the agent changes the current state of the world, based on state of the world and knowledge (memory), it triggers actions through the effectors"];
+answer = ["Model based reflex agent"];
+group = "Agent Architectures";
+picture = "img/modelbased.png";
+items.push({question,answer,group,picture});
+
+question = ["The knowledge of the current state environment is not always sufficient to decide for an agent to what to do. and it needs to know its goal."];
+answer = ["Goal based agent"];
+group = "Agent Architectures";
+picture = "img/goalbased.png";
+items.push({question,answer,group,picture});
+
+question = ["it provides a more general agent framework. The agent acts so as to maximize expected utility."];
+answer = ["Utility based agent"];
+group = "Agent Architectures";
+picture = "img/utilitybased.png";
+items.push({question,answer,group,picture});
+
+question = ["A learning agent in AI is the type of agent which can learn from its past experiences, or it has learning capabilities."];
+answer = ["Learning Agent"];
+group = "Agent Architectures";
+picture = "img/learnagent.png";
+items.push({question,answer,group,picture});
+
+question = ["Who defined the Artificial Intelligence in 1956?"];
+answer = ["McCarthy"];
+group = "Definition";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["A system with intelligence is expected to behave as intelligently as what?"];
+answer = ["Human"];
+group = "Definition";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["is concerned with the design of intelligence in an artificial device.","Is about designing systems that are as intelligent as humans.","involves trying to understand human thought and an effort to build machines that emulate the human thought process."];
+answer = ["Artificial Intelligence"];
+group = "Definition";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["it is somethat that the machine has to fool the interrogator into believing that it is human."];
+answer = ["Turing Test"];
+group = "Definition";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["It deals with studies of ideal or rational thought process and inference."];
+answer = ["Logic and laws of thought"];
+group = "Definition";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["It deals with building machines that act rationally.","It deals with how the system acts and performs, and not so much on the reasoning process."];
+answer = ["Study of rational agents"];
+group = "Definition";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+//  AAAAAAAAAAAAAA   //
+
+question = ["What kind of tasks are these for the AIs?"];
+answer = ["Common-place tasks","Common place tasks","Common tasks"];
+group = "Tasks of an Intelligent entity";
+picture = "img/common.png";
+items.push({question,answer,group,picture});
+
+question = ["What kind of tasks are these for the AIs?"];
+answer = ["Expert tasks"];
+group = "Tasks of an Intelligent entity";
+picture = "img/expert.png";
+items.push({question,answer,group,picture});
+
+question = ["What are these?"];
+answer = ["Practical use of AI"];
+group = "Tasks of an Intelligent entity";
+picture = "img/practical.png";
+items.push({question,answer,group,picture});
+
+// TYPES OF AI // 
+
+question = ["Aims to build machines that can truly reason and solve problems."];
+answer = ["Strong AI"];
+group = "Types of AI";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["Deals with the creation of some form of computer-based artificial intelligence that cannot truly reason and solve problems, but can act as if it were intelligent."];
+answer = ["Weak AI"];
+group = "Types of AI";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["Aims to produce commercially viable 'smart' systems."];
+answer = ["Applied AI"];
+group = "Types of AI";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+question = ["These computers are used to test theories about how the human mind works, what is this?"];
+answer = ["Cognitive AI"];
+group = "Types of AI";
+picture = "img/";
+items.push({question,answer,group,picture});
+
+/* exclusion from remix */
+var DontIncludeFromRemix1 = [""];
+var DontIncludeFromRemix2 = [""];
+var DontIncludeFromRemix3 = [""];
+
+/* game configuration */
+var identificationMode = false;
+var drainBy = -0.005;
+var wrongDamage = 25;
+var examMode = false; // no practice mode 
+var PanicMode = false; // with health and timer
+var RemixModeRarity = 5; // the lower the value, the more remixes occuring | if 0 then all will be remix mode
+
+
+/* level management */
+var DontIncludeGroups = [""];
+
+/* music configuration*/
+var bgmusic0_PF = 'seriousSoundtrack/bgmusic0.mp3';
+var bgmusic1_PF = 'seriousSoundtrack/bgmusic1.mp3';
+var bgmusic2_PF = 'seriousSoundtrack/bgmusic2.mp3';
+var bgmusic3_PF = 'seriousSoundtrack/bgmusic3.mp3'; 
+var bgmusic4_PF = 'seriousSoundtrack/bgmusic4.mp3'; 
+var noMusic = ftrue; // noMusic in practice mode??
